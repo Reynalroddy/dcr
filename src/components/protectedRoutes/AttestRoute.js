8d959e85466ebe689b6e23244f9c4d5b7520bc25
Reynalroddy <58977649+Reynalroddy@users.noreply.github.com>
2023-05-10 11:09:17 +0100

    dis

--- src/components/protectedRoutes/AttestRoute.js
+++ src/components/protectedRoutes/AttestRoute.js
@@ -2,31 +2,40 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 export const AttestRoute = ({ children }) => {
-  const { userInfo } = useSelector((state) => state.user);
+  // const { userInfo } = useSelector((state) => state.user);
+  const userInfo = JSON.parse(localStorage.getItem('user-birth'))
 return (userInfo?.permission?.permissions.includes('Can_List_Attestation') && userInfo?.permission?.permissions.includes('Can_View_Attestation'))
 ?children: <Navigate to="/unauth" />;
 };
 
 export const BirthRegRoute = ({ children }) => {
-    const { userInfo } = useSelector((state) => state.user);
+    // const { userInfo } = useSelector((state) => state.user);
+    const userInfo = JSON.parse(localStorage.getItem('user-birth'))
   return (userInfo?.permission?.permissions.includes('Can_List_Birth') && userInfo?.permission?.permissions.includes('Can_View_Birth'))
   ?children: <Navigate to="/unauth" />;
   };
 
 
   export const DeathRegRoute = ({ children }) => {
-    const { userInfo } = useSelector((state) => state.user);
+    // const { userInfo } = useSelector((state) => state.user);
+    const userInfo = JSON.parse(localStorage.getItem('user-birth'))
   return (userInfo?.permission?.permissions.includes(' Can_List_Death') && userInfo?.permission?.permissions.includes('Can_View_Death'))
   ?children: <Navigate to="/unauth" />;
   };
 
   export const StillRegRoute = ({ children }) => {
-    const { userInfo } = useSelector((state) => state.user);
+    // const { userInfo } = useSelector((state) => state.user);
+    const userInfo = JSON.parse(localStorage.getItem('user-birth'))
   return (userInfo?.permission?.permissions.includes('Can_List_Stillbirth') && userInfo?.permission?.permissions.includes('Can_View_Stillbirth'))
   ?children: <Navigate to="/unauth" />;
   };
+
+      
+     
+
   export const ModRegRoute = ({ children }) => {
-    const { userInfo } = useSelector((state) => state.user);
+    // const { userInfo } = useSelector((state) => state.user);
+    const userInfo = JSON.parse(localStorage.getItem('user-birth'))
   return (userInfo?.permission?.permissions.includes('Can_List_Modification') && userInfo?.permission?.permissions.includes('Can_View_Modification'))
   ?children: <Navigate to="/unauth" />;
   };
